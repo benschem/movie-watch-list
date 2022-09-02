efba1194fd68114f9f83576b2f426ff56bbb9d06
Ben Schembri <benschembri@gmail.com>
2022-09-02 17:04:08 +1000

    added rating

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
